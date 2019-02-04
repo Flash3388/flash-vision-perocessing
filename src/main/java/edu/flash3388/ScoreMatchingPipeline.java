33674efdb8fc00d8cdc85d870eb6eb67f2a6b20b
alon <skrops.a@gmail.com>
2019-02-04 22:30:11 +0200

    final changes to work in real time

--- src/main/java/edu/flash3388/ScoreMatchingPipeline.java
+++ src/main/java/edu/flash3388/ScoreMatchingPipeline.java
@@ -3,6 +3,7 @@ package edu.flash3388;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
@@ -59,7 +60,6 @@ public class ScoreMatchingPipeline implements VisionPipeline {
 	@Override
 	public void process(Mat image) {
 		try {
-			image = Imgcodecs.imread("/home/pi/templates/templ2019.jpg");
 			// will use this to perform vision processing, so that the original image
 			// remains intact to draw info on it
 			Mat hsvImage = new Mat();
@@ -75,27 +75,32 @@ public class ScoreMatchingPipeline implements VisionPipeline {
 			 */
 			Range hue = new Range(0, 180);
 			Range saturation = new Range(0, 255);
-			Range value = new Range(200, 255);
+			Range value = new Range(220, 255);
 
 			/*
 			 * Filter the image by color range
 			 */
 			mCvProcessing.filterMatColors(hsvImage, hsvImage, hue, saturation, value);
 			List<MatOfPoint> contours = mCvProcessing.detectContours(hsvImage);
-			System.out.println(String.format("procces %d", contours.size()));
 						
 			List<RotatedRect> rects = new ArrayList<RotatedRect>();
+			Mat pushImage = new Mat();
+			Imgproc.cvtColor(hsvImage, pushImage, Imgproc.COLOR_GRAY2RGB);
 			for (MatOfPoint c : contours) {
+					
 				MatOfPoint2f cnt2f = new MatOfPoint2f(c.toArray());
+			
 				RotatedRect rect = Imgproc.minAreaRect(cnt2f);
-				drawRotatedRect(image, rect);
-				
-				if(rect.size.width < rect.size.height)
-					rect.angle += 180; 
-				else
-					rect.angle += 90;
-
-				rects.add(rect);
+				if(rect.boundingRect().area() > 10.0)
+				{
+					drawRotatedRect(pushImage, rect);	
+					if(rect.size.width < rect.size.height)
+						rect.angle += 180; 
+					else
+						rect.angle += 90;
+	
+					rects.add(rect);
+				}
 			}
 			
 			
@@ -103,12 +108,11 @@ public class ScoreMatchingPipeline implements VisionPipeline {
 			for(RotatedRect rect1 : rects)
 				for(RotatedRect rect2 : rects){
 					if(rect1.center != rect2.center){ // not same rect
-						double score = ((rect1.angle + rect2.angle)%360) / 180.0; //angle sum should be 180
+						double score = ((rect1.angle + rect2.angle) %360) / 180.0; //angle sum should be 180
 						if(score > 1.0)
 							score = 1.0 - (score - 1.0); // lats pray
 						pairs.add(new RectsPair(rect1, rect2, score));
 					}
-				
 				}
 			
 			System.out.println(pairs.size());
@@ -118,32 +122,13 @@ public class ScoreMatchingPipeline implements VisionPipeline {
 					bestPair = pair;
 			if(bestPair != null)
 			{
-				System.out.println("we got best");
-				System.out.println();
-				Imgproc.circle(image, new Point((bestPair.rect2.center.x + bestPair.rect1.center.x)/2.0,(bestPair.rect2.center.y + bestPair.rect1.center.y)/2.0), DRAW_CIRCLE_RADIUS, new Scalar(0, 255, 0));
+				System.out.println(String.format("best score - %f", (float)bestPair.score));
+				Imgproc.circle(pushImage, new Point((bestPair.rect2.center.x + bestPair.rect1.center.x)/2.0,(bestPair.rect2.center.y + bestPair.rect1.center.y)/2.0), DRAW_CIRCLE_RADIUS, new Scalar(0, 255, 0));
 			}
 
-			if(!write){		
-				System.out.println("writing image");
-				write = true;
-				Imgcodecs.imwrite("/home/pi/res1.jpg", image);
-			}
-/*
-			for(int i=0; i < rects.size(); i++)
-			{
-				Imgproc.drawContours(image, rects, i, new Scalar(255, 0, 0));
-			}
+			mResultOutput.putFrame(pushImage);
 			
-			if(!write){		
-				System.out.println("writing image");
-				write = true;
-				Imgcodecs.imwrite("/home/pi/res1.jpg", image);
-			}
-			*/
-			//Imgproc.drawContours(hsvImage, rects, 0, new Scalar(51, 51, 51));
-			//System.out.println("gone that far");
-			
-			//mResultOutput.putFrame(hsvImage);
+			/*
 			// drawResult(image, result);
 
 			// TODO: ADD CODE TO WRITE OUTPUT INTO TABLE. MAY ALSO WRITE INFORMATION ABOUT
@@ -183,10 +168,10 @@ public class ScoreMatchingPipeline implements VisionPipeline {
 	}
 
 	private void drawBox(Mat image, Point p1, Point p2, Point p3, Point p4) {
-		Imgproc.line(image, p1, p2, DRAW_CIRCLE_COLOR);
-		Imgproc.line(image, p2, p3, DRAW_CIRCLE_COLOR);
-		Imgproc.line(image, p3, p4, DRAW_CIRCLE_COLOR);
-		Imgproc.line(image, p4, p1, DRAW_CIRCLE_COLOR);
+		Imgproc.line(image, p1, p2, DRAW_CIRCLE_COLOR, 2);
+		Imgproc.line(image, p2, p3, DRAW_CIRCLE_COLOR, 2);
+		Imgproc.line(image, p3, p4, DRAW_CIRCLE_COLOR, 2);
+		Imgproc.line(image, p4, p1, DRAW_CIRCLE_COLOR, 2);
 
 	}
 
