128971db7a5e96bdaa4c26b6f27afafa236dc37c
Tom Tzook <tomtzook@gmail.com>
2020-02-05 22:04:09 +0200

    deployment update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-scp build/libs/java-multiCameraServer-all.jar pi@frcvision.local:/home/pi
+scp build/distribution/frcvision.zip flash@flash-jetson.local:/home/flash
 scp runCamera pi@frcvision.local:/home/pi
+ssh flash@flash-jetson.local rm -r /home/flash/frcvision && unzip /home/flash/frcvision.zip
